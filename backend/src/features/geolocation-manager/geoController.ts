42c50bb689372a3a6a92569dbe77eaa6ab154e2a
Stanislav Lukaniev <staslukanev@gmail.com>
2025-05-01 19:19:18 +0200

    Error handling & Types/Interfaces refactoring: Geolocation-manager
    Backend

--- backend/src/features/geolocation-manager/geoController.ts
+++ backend/src/features/geolocation-manager/geoController.ts
@@ -1,26 +1,22 @@
 import axios from "axios";
 import { Request, Response, NextFunction } from "express";
 
-export const getUserLocation = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
+import ApiError from "../../utils/apiError.js";
+import { asyncHandler } from "../../utils/asyncHandler.js";
+
+export const getUserLocation = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
     // Extract the client's IP address from the request headers
     const clientIp =
       req.headers["x-forwarded-for"]?.toString().split(",")[0] || // For proxies/load balancers
       req.socket.remoteAddress; // Fallback to direct connection IP
 
     if (!clientIp) {
-      res.status(400).json({ error: "Unable to determine client IP address" });
-      return;
+      return next(new ApiError(400, "Unable to determine client IP address"));
     }
 
     // Fetch the geolocation data using the client's IP address
     const geoResponse = await axios.get(`https://ipapi.co/${clientIp}/json/`);
     res.json(geoResponse.data);
-  } catch (error) {
-    next(error);
   }
-};
+);
